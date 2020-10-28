827d29fa8fd48866062ee6517edf2c4097dc41d7
Utha89 <mcrclaudio08@gmail.com>
2020-10-28 22:54:55 +0700

    cara bikin variabel dan tipe data

---
+++ tipe_data/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//belajar bikin variabel dengan tipe data
+
+	//1.
+	var nama string = "utha"
+	//2 bisa tanpa var bisa dengan
+	age := 20
+	fmt.Println("namanya adalah %s" + nama)
+	fmt.Println(age)
+}
