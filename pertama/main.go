e6ebd908cebdc199a6a3ed363d88881c761f1273
Utha89 <mcrclaudio08@gmail.com>
2020-10-28 21:07:41 +0700

    tentang package main serta bkin 2file yg sama2 pacakge main dan buat cara jalanin yaitu di panggil function di package main lainnya

--- pertama/main.go
+++ pertama/main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	//cara panggil function yg masih sama2 1 pacake yaitu package main
+	kalimat := TestAja()
+
+	fmt.Println(kalimat)
 }
