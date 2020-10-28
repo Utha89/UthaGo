d615a2c2efc301f6ff8ad3293bb85e8504aad8e2
Utha89 <mcrclaudio08@gmail.com>
2020-10-28 21:42:37 +0700

    balikin lg yg menggunakan function public

--- pertama/calculation/add.go
+++ pertama/calculation/add.go
@@ -1,13 +1,14 @@
 package calculation
 
-//Funtion add yg membuthkakn sebuah parameter
+//add yg membuthkakn sebuah parameter
 //nama parameter nya number tipe int
 //dimna kembaliannya adalah berupa int juga
 func Add(number int, numberTwo int) int {
-	//return number + numberTwo
-	return add(number, numberTwo)
+	return number + numberTwo
+	//return add(number, numberTwo)
 }
 
+//Sifatnya private
 func add(number int, numberTwo int) int {
 	return number + numberTwo
 }
