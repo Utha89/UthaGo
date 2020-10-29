c12ed9f23899da2568b23fd1417a9b8e24cfd84e
Utha89 <mcrclaudio08@gmail.com>
2020-10-29 22:51:36 +0700

    go dengan array for each yg ambil data dr array

--- go_array/main.go
+++ go_array/main.go
@@ -29,17 +29,40 @@ func main() {
 
 	fmt.Println(kata)
 	fmt.Println(bahasa)
-
+	fmt.Println("______________________")
 	//Untuk mengetahui panjang array
 	panjangKata := len(bahasa)
 	fmt.Println(panjangKata)
-
+	fmt.Println("______________________")
 	//Just info untuk hitung jumlah array atau nentuin jumlah array kan yg udah2
 	//[5] di go bisa [...]
-
 	//nah klo nulis array vertikal terakhirnya tetep pake , klo horizontal tnpa koma
+	//examplenya
 
+	kalimat := [...]string{
+		"Flutter",
+		"PHP",
+		"Go",
+		"Dart",
+		"Java",
+	}
+	fmt.Println("trik nampilin array model vertikal")
+	fmt.Println("kalimatnya:", kalimat)
 	//nah klo di php lnjutin dr string pake . contoh : $utha."saya"
 	//nah klo di flutter(dart) dan JS lnjutin dr string pake + contoh : "saya" + utha
 	//nah klo di Golang lnjutin dr string pake , contoh : "saya", utha
+
+	//Nampilin array lalu kita for each
+	fmt.Println("______________________")
+	sepakBola := [...]string{
+		"JUVE",
+		"BARCA",
+		"Timnas u19",
+	}
+
+	fmt.Println("for each yg ambil data dr array :")
+	for index, kal := range sepakBola {
+
+		fmt.Println("indexnya", index, "Club nya", kal)
+	}
 }
