efa36c2ef82a62546618ba61d54093cc48f8585b
Utha89 <mcrclaudio08@gmail.com>
2020-10-29 20:33:56 +0700

    go dengan for dan for each

---
+++ go_for/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		fmt.Println("angka", i)
+	}
+
+	//for each
+	title := "utha ganteng"
+	for index, letter := range title {
+		fmt.Println("index", index, "letter", letter)
+	}
+}
