8e844a9f813d361aea7ff5761cfbfb56de7ce051
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 16:31:19 +0700

    slice yg udah ada isi array nya

--- go_slice/main.go
+++ go_slice/main.go
@@ -5,11 +5,15 @@ import (
 )
 
 func main() {
-	var gameConsole []string
 
-	gameConsole = append(gameConsole, "PS5")
-	gameConsole = append(gameConsole, "PS2")
-	gameConsole = append(gameConsole, "PS3")
+	//bisa juga slice yg udah ada isi array nya
+	gameConsole := []string{"PS5", "PS2", "PS3"}
+
+	// var gameConsole []string
+
+	// gameConsole = append(gameConsole, "PS5")
+	// gameConsole = append(gameConsole, "PS2")
+	// gameConsole = append(gameConsole, "PS3")
 
 	fmt.Println(gameConsole)
 }
