44194461b75eb32766792f48a8d885348f356119
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 16:57:31 +0700

    klo di array kan kita bisa fmt.Println(nama[0]) klo di map kita bisa panggil indexnya / key nya

--- go_map/main.go
+++ go_map/main.go
@@ -15,4 +15,9 @@ func main() {
 	var nama string = "utha"
 
 	fmt.Println(nama)
+
+	//cara manggil suatu nilai map
+	//klo di array kan kita bisa 	fmt.Println(nama[0])
+	//klo di map kita bisa panggil indexnya / key nya
+	fmt.Println(myMap["ps5"])
 }
