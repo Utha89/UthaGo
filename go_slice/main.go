fab0558f04751b31e6ddf837290dea5687a5e80c
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 16:34:08 +0700

    slice yg ada for each nya

--- go_slice/main.go
+++ go_slice/main.go
@@ -7,13 +7,17 @@ import (
 func main() {
 
 	//bisa juga slice yg udah ada isi array nya
-	gameConsole := []string{"PS5", "PS2", "PS3"}
+	//gameConsole := []string{"PS5", "PS2", "PS3"}
 
-	// var gameConsole []string
+	var gameConsole []string
 
-	// gameConsole = append(gameConsole, "PS5")
-	// gameConsole = append(gameConsole, "PS2")
-	// gameConsole = append(gameConsole, "PS3")
+	gameConsole = append(gameConsole, "PS5")
+	gameConsole = append(gameConsole, "PS2")
+	gameConsole = append(gameConsole, "PS3")
 
-	fmt.Println(gameConsole)
+	//pakai for each
+	for _, console := range gameConsole {
+		fmt.Println(console)
+	}
+
 }
