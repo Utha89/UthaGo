7f24bf10d0662ebda326f9a901a728ff18d4b355
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 17:07:59 +0700

    Map bisa juga dibikin kaya aray bikin variabel lg bisa di isi valuenya

--- go_map/main.go
+++ go_map/main.go
@@ -20,4 +20,9 @@ func main() {
 	//klo di array kan kita bisa 	fmt.Println(nama[0])
 	//klo di map kita bisa panggil indexnya / key nya
 	fmt.Println(myMap["ps5"])
+
+	//Map bisa juga dibikin kaya aray bikin variabel lg bisa di isi valuenya
+	myGame := map[int]string{1: "PS3", 2: "PS5", 3: "PS1"}
+	fmt.Println(myGame)
+
 }
