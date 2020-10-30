e5af8c94251c779caec32b5f61596d76ee263d1f
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 16:15:38 +0700

    menggunakan slice untuk mengisi suatu array dengan menggunakan append

---
+++ go_slice/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var gameConsole []string
+
+	gameConsole = append(gameConsole, "PS5")
+	gameConsole = append(gameConsole, "PS2")
+	gameConsole = append(gameConsole, "PS3")
+
+	fmt.Println(gameConsole)
+}
