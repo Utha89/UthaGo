a43cba78ce4f1737663b84daf79ef0bb106a7699
Utha89 <mcrclaudio08@gmail.com>
2020-10-30 16:53:55 +0700

    map disini ialah bisa mentukan key nya sendiri bisa integer atau string dalam suatu array

--- go_map/main.go
+++ go_map/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	//var myMap map[string]int{}
+	myMap := map[string]int{}
+
+	myMap["ruby"] = 10000
+	myMap["ps5"] = 90000000
+	fmt.Println(myMap)
+
+	var nama string = "utha"
+
+	fmt.Println(nama)
 }
