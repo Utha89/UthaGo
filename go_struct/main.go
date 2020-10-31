cf4803a0cbb480b13ce476580595b3c737e8fa82
Utha89 <mcrclaudio08@gmail.com>
2020-10-31 20:18:32 +0700

    pembuatan struct / model sama seperti kaya di flutter tp namnya sich struct

--- go_struct/main.go
+++ go_struct/main.go
@@ -16,13 +16,20 @@ import (
 
 func main() {
 	//cara panggil model bikin variabel dan nama modelnya
-	user :=User{}
+	user :=User{
+		ID:1,
+		FisrtName:"Utha",
+		LastName:"Ganteng",
+		Email:"a@yahuu.com",
+		IsActive:true,
+		
+	}
 
-	user.ID=1
-	user.FisrtName="Utha"
-	user.LastName="ganteng"
-	user.Email="a@email.com"
-	user.IsActive=true
+	// user.ID=1
+	// user.FisrtName="Utha"
+	// user.LastName="ganteng"
+	// user.Email="a@email.com"
+	// user.IsActive=true
 
 	user2 :=User{}
 	user2.ID=2
