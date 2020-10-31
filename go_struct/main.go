35db80113c41027ea1a922ec333143356e84f4d0
Utha89 <mcrclaudio08@gmail.com>
2020-11-01 00:41:05 +0700

    pembuatan function yg model didalemnya ada model nya lg

--- go_struct/main.go
+++ go_struct/main.go
@@ -25,19 +25,19 @@ type Group struct{
 func main() {
 	//cara panggil model bikin variabel dan nama modelnya
 	user :=User{ID:1,FisrtName:"Utha",LastName:"Ganteng",Email:"a@yahuu.com",IsActive:true}
-
+	user2 :=User{ID:1,FisrtName:"Utha",LastName:"Ganteng",Email:"a@yahuu.com",IsActive:true}
 	// user.ID=1
 	// user.FisrtName="Utha"
 	// user.LastName="ganteng"
 	// user.Email="a@email.com"
 	// user.IsActive=true
 
-	user2 :=User{}
-	user2.ID=2
-	user2.FisrtName="Utha 2"
-	user2.LastName="ganteng 2"
-	user2.Email="a2@email.com"
-	user2.IsActive=false
+	// user2 :=User{}
+	// user2.ID=2
+	// user2.FisrtName="Utha 2"
+	// user2.LastName="ganteng 2"
+	// user2.Email="a2@email.com"
+	// user2.IsActive=false
 
 	fmt.Println(user)
 	fmt.Println(user2.FisrtName)
